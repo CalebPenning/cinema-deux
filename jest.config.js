224f6265924577b9fbc53c21fbe7911c0f629eda
Caleb Penning <caleb.penning@accenture.com>
2022-06-02 21:27:47 -0700

    Initial commit: install all (known) dependencies, rearrange folder structure and file extensions, get text on screen

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+import nextJest from 'next/jest'
+
+const createJestConfig = nextJest({
+  dir: './'
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverage: true,
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/pages/$1'
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['cypress/']
+}
+
+module.exports = createJestConfig(customJestConfig)
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: ['.'],
+  pageExtensions: ['page.tsx', 'api.ts']
 }
 
 module.exports = nextConfig
