3426aa54127a6feb7b86c9f45e679dce9d19b89d
Caleb Penning <calebpenning@mail.com>
2022-06-16 00:00:07 -0700

    feat/add-google-auth: add placeholder ToS page

---
+++ src/pages/terms-of-service/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next"
+
+const TermsOfService: NextPage = () => {
+	return (
+		<div>
+			<h1>Terms of Service</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod,
+				nisi vel consectetur euismod, nisl nunc euismod nisi, euismod.
+			</p>
+		</div>
+	)
+}
+
+export default TermsOfService
