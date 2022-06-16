b55621bb42ab57a68e98ade8fbecbabc1bc779a6
Caleb Penning <calebpenning@mail.com>
2022-06-16 00:07:19 -0700

    feat/add-google-auth: add placeholder privacy policy page

---
+++ src/pages/privacy-policy/index.tsx
@@ -0,0 +1,12 @@
+const PrivacyPolicy = () => {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod,
+            </p>
+        </div>
+    )
+}
+
+export default PrivacyPolicy
